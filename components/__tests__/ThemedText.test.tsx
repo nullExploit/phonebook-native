389aeb81a35d1859928f090606c7285ca348c851
Rishad Aufa Harahap <rishadaufaharahap@gmail.com>
2024-08-16 23:20:11 +0700

    Remove Unnecessary Test Unit And Snapshot

--- components/__tests__/ThemedText.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react-native';
-import { ThemedText } from '../ThemedText';
-
-it(`renders correctly`, () => {
-  const tree = render(<ThemedText>Snapshot test!</ThemedText>).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
